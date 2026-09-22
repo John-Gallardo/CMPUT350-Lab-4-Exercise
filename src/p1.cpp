0a2167f4e3f36c0bf23544724abca0dc6c78712b
John Gallardo <johnpatrickgallardo33@gmail.com>
2026-09-22 14:27:28 -0600

    feat: add test code

--- src/p1.cpp
+++ src/p1.cpp
@@ -1,1 +1,36 @@
-int main() { return 0; }
+#include "timer.h"
+#include <random>  // for mersenne twister
+#include <array>
+#include <cstdint>
+#include <iostream>
+
+constexpr int g_arraySize{4000};
+
+int main() {
+    std::mt19937_64 rng(0);  // TODO: segfaults here for some reason SPECIFICALLY for debug
+
+    // Test 1: Row Major Traversal
+    Timer timer{};
+    std::array<std::array<int, g_arraySize>, g_arraySize> rowMajorMatrix{};
+    timer.restart();
+    for (int i{0}; i < g_arraySize; i++) {
+        for (int j{0}; j < g_arraySize; j++) {
+            rowMajorMatrix[i][j] = rng();
+        }
+    }
+    std::cout << "Row Major Traversal: " << timer.glance<Timer::Micros>() << '\n';
+
+    // Test 2: Col Major Traversal
+    rng.seed(0);
+    std::array<std::array<int, g_arraySize>, g_arraySize> colMajorMatrix{};
+    timer.restart();
+    for (int i{0}; i < g_arraySize; i++) {
+        for (int j{0}; j < g_arraySize; j++) {
+            colMajorMatrix[j][i] = rng();
+        }
+    }
+    std::cout << "Col Major Traversal: " << timer.glance<Timer::Micros>() << '\n';
+
+}
+
+
--- src/timer.h
+++ src/timer.h
@@ -1,6 +1,47 @@
 #ifndef TIMER_H
 #define TIMER_H
+#include <chrono>
+#include <cstdint>
 
-// replace with yours from the prep...
+class Timer {
+    public:
+        // aliases
+        using Nanos   = std::chrono::nanoseconds;
+        using Micros  = std::chrono::microseconds;
+        using Millis  = std::chrono::milliseconds;
+        using Seconds = std::chrono::seconds;
+        using Minutes = std::chrono::minutes;
+        using Hours   = std::chrono::hours;
+
+        // constructor
+        Timer()
+        : m_time{std::chrono::steady_clock::now()} {}
+
+        // resets state of timer
+        void restart() {
+            m_time = std::chrono::steady_clock::now();
+        }
+
+        // returns time unit T since timer was last clicked/restarted/constructed
+        // NOTE: keeping track of a separate timer for click/restart/constructed seemed redundant
+        // because we only need to keep the most recent one so there is only m_time.
+        template <typename T>
+        uint64_t click() {
+            auto timeNow{std::chrono::steady_clock::now()};
+            // static cast to stop warning
+            uint64_t timeElapsed{static_cast<uint64_t>(std::chrono::duration_cast<T>(timeNow - m_time).count())};
+            restart();
+            return timeElapsed;
+        }
+
+        // literally the same as click() but doesn't restart time
+        template <typename T>
+        uint64_t glance() const {
+            auto timeNow{std::chrono::steady_clock::now()};
+            return std::chrono::duration_cast<T>(timeNow - m_time).count();
+        }
+    private:
+        std::chrono::time_point<std::chrono::steady_clock> m_time{};
+};
 
 #endif  // TIMER_H
