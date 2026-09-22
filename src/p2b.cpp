8bc0c829c44e6b352b902b85da7c46dfeae29302
John Gallardo <johnpatrickgallardo33@gmail.com>
2026-09-22 15:58:05 -0600

    feat: finish filling morton order array

--- src/p2b.cpp
+++ src/p2b.cpp
@@ -6,6 +6,11 @@
 
 constexpr int g_arraySize{256};
 
+int rowMajorIndexA(int x, int y, int z) {
+    // this should be the correct formula?
+    return (x * g_arraySize * g_arraySize) + (y * g_arraySize) + z;
+}
+
 int main() {
     Timer timer{};
     std::mt19937_64 rng(0);
@@ -16,14 +21,22 @@ int main() {
     for (int i{0}; i < g_arraySize; i++) {
         for (int j{0}; j < g_arraySize; j++) {
             for (int k{0}; k < g_arraySize; k++) {
-                // this should be the correct formula?
-                rowMajorArray[(i * g_arraySize * g_arraySize) + (j * g_arraySize) + k] = rng();
+                rowMajorArray[rowMajorIndexA(i, j, k)] = rng();
             }
         }
     }
     std::cout << timer.glance<Timer::Micros>() << '\n';
     
     // B: Morton-order array
-
+    std::vector<uint64_t> mortonArray(g_arraySize * g_arraySize * g_arraySize, 0);
+    timer.restart();
+    for (int i{0}; i < g_arraySize; i++) {
+        for (int j{0}; j < g_arraySize; j++) {
+            for (int k{0}; k < g_arraySize; k++) {
+                mortonArray[morton3d(i, j, k)] = rowMajorArray[rowMajorIndexA(i, j, k)];
+            }
+        }
+    }
+    std::cout << timer.glance<Timer::Micros>() << '\n';
     return 0;
 }
